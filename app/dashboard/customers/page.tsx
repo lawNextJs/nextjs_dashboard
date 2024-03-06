68d31b181074d063ab24144c743e68d13a242d45
noname <cloudeven@gmail.com>
2024-03-06 23:52:16 +0900

    .

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -3,5 +3,5 @@ import { Metadata } from 'next'
 export const metadata: Metadata = { title: 'Customers' }
 
 export default function Page() {
-    return <p>Customers Page</p>
+    return <p>Customers Page~</p>
 }
